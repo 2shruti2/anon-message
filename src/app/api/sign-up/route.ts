408e5098432ab1a7771283d3b0902b53483213bd
2shruti2 <shruthesharma@gmail.com>
2024-06-27 00:13:15 +0530

    new user creation

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -3,30 +3,61 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
 
-export async function POST (request : Request) {
-  await dbConnect
+export async function POST(request: Request) {
+  await dbConnect;
 
   try {
-    const {username, email, password} = await request.json()
+    const { username, email, password } = await request.json();
     const existingUserVerifiedByUsername = await UserModel.findOne({
       username,
-      isVerified: true
-    })
+      isVerified: true,
+    });
 
-    if(existingUserVerifiedByUsername){
-      return Response.json({
-        
-      })
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        {
+          status: 400,
+        }
+      );
     }
 
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      true;
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiryDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+
+    }
   } catch (error) {
-    console.log("Error registering user", error) // for our terminal 
-    return Response.json({  // for frontend
-      success: false,
-      message: "Error registering user"
-    }, 
-  {
-    status: 500
-  })
+    console.log("Error registering user", error); // for our terminal
+    return Response.json(
+      {
+        // for frontend
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 500,
+      }
+    );
   }
 }
